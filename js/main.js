56328ae8c4961fd55a71be1021cc8f5833250589
Samantha Casseus <47072462+samcassdev@users.noreply.github.com>
2019-03-01 08:52:36 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,64 +1,63 @@
 var total = 100;
 
-document.getElementById('minBet').addEventListener("click", function(){
-  //set reel values
-  var reel1 = randomReel()
-  var reel2 = randomReel()
-  var reel3 = randomReel()
-  //console.log(reel1,reel2,reel3)
-  //conditional to update total value
-  if( checkMatch(reel1,reel2,reel3) ){
+document.getElementById('minimumBet').addEventListener("click", function(){
+  //set values for each reel
+  var firstReel = randomTotal()
+  var secondReel = randomTotal()
+  var thirdReel = randomTotal()
+  //console.log firstReel, secondReel, and thirdReel
+  //conditional -- update the total value
+  if( totalEquals(firstReel, secondReel, thirdReel) ){
     total = total + 25;
   }else{
     total = total - 1;
   }
-  //update dom with new total
+  //reveal the new total in the DOM
   document.getElementById('total').innerHTML = total;
 });
 
-document.getElementById('maxBet').addEventListener("click", function(){
-  //set reel values
-  // conditional to update total value
-  var reel1 = randomReel()
-  var reel2 = randomReel()
-  var reel3 = randomReel()
-  //console.log(reel1,reel2,reel3)
-  //conditional to update total value
-  if( checkMatch(reel1,reel2,reel3) ){
-    total = total + 250;
+document.getElementById('maximumBet').addEventListener("click", function(){
+  //set values for each reel
+  //conditional -- update the total value
+  var firstReel = randomTotal()
+  var secondReel = randomTotal()
+  var thirdReel = randomTotal()
+  //console.log firstReel, secondReel, and thirdReel
+  //conditional -- update the total value
+  if( totalEquals(firstReel, secondReel, thirdReel)){
+    total = total + 150;
   }else{
     total = total - 25;
   }
-  //update dom with new total
+  //update the DOM with the total value
   document.getElementById('total').innerHTML = total;
 });
 
-//choose random value for the reel
-function randomReel(){
-  // 1 ut of 5
+//set random values for each reel
+function randomTotal(){
 
-  var choice = Math.random()
-  if (choice <= .2){
-    choice = "cherry"
-  }else if(choice <= .4){
-    choice = "bar"
-  }else if(choice <= .6){
-    choice = "seven"
-  }else if(choice <= .8){
-    choice = "bell"
+  var result = Math.random()
+  if (result <= .3){
+    result = "Gryffindor"
+  }else if(result <= .6){
+    result = "Hufflepuff"
+  }else if(result <= .9){
+    result = "Ravenclaw"
+  }else if(result <= .12){
+    result = "Slytherin"
   }else{
-    choice = "moneyBag"
+    result = "Silly Muggle"
   }
-  //return the value
-  return choice;
+  //update the DOM with the sorting lottery result
+  document.getElementById('sorting').innerHTML = result;
+
 
 }
-//compare reels to find if they are a winner
-function checkMatch(reel1,reel2,reel3){
-  if( reel1 === reel2 && reel1 === reel3 ){
+//compare each reel to assist with making the sorting decision
+function totalEquals(firstReel, secondReel, thirdReel){
+  if( firstReel === secondReel && firstReel === thirdReel ){
     return true
   }else{
     return false
   }
 }
-//return true or false
